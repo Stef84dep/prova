11701a18def1ac86129edea38196aea3179aefee
Stef84dep <81813836+Stef84dep@users.noreply.github.com>
2022-01-26 17:28:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function Picker() {
 export default function App() {
   return (
     <>
-      <VRCanvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 100}}>
+      <ARCanvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 100}}>
         <ambientLight intensity={0.7} />
         <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
         <Suspense fallback={null}>
@@ -93,7 +93,7 @@ export default function App() {
         <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
         <DefaultXRControllers/>
         <Hands/>
-      </VRCanvas>
+      </ARCanvas>
       <Picker />
     </>
   )
